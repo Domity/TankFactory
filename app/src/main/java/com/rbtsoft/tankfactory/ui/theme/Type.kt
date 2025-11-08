5e140d0e16feceea425876cdc1d7d7e6a6449bd2
Domity <188688780+Domity@users.noreply.github.com>
2025-11-08 22:20:11 +0800

    更大的标题

--- app/src/main/java/com/rbtsoft/tankfactory/ui/theme/Type.kt
+++ app/src/main/java/com/rbtsoft/tankfactory/ui/theme/Type.kt
@@ -13,5 +13,12 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp
     )
 )

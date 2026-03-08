13b277b3b66f56dac8a6b5852bdc37e0e87de938
Domity <188688780+Domity@users.noreply.github.com>
2026-03-08 11:28:40 +0800

    移除coil库

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,7 +99,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
-    implementation(libs.coil.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.compose.foundation)
 }

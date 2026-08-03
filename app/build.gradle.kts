fcaa427217a544c5c45b7962bd95a526cdd69408
Domity <188688780+Domity@users.noreply.github.com>
2026-08-03 15:46:33 +0800

    1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.domity.tankfactory"
         minSdk = 31
         targetSdk = 36
-        versionCode = 112
-        versionName = "1.1.2"
+        versionCode = 113
+        versionName = "1.1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations += setOf("zh")
         externalNativeBuild {
